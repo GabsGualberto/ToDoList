0440aef194e441f31f8233f5377422b711bb8446
gabirel <gabrielgualbertooliveira@gmail.com>
2025-10-18 15:03:45 -0300

    feat: config swagger

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -14,6 +14,21 @@ builder.Services.AddSingleton<IAssignmentRepository, AssigmentRepository>();
 
 builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(ApplicationMarker).Assembly));
 
+builder.Services.AddSwaggerGen(configuration =>
+{
+    configuration.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "ToDoList API",
+        Version = "v1"
+    });
+
+    configuration.AddServer(new Microsoft.OpenApi.Models.OpenApiServer()
+    {
+        Description = "Localhost",
+        Url = "https://localhost:5000/"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
