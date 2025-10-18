687f39809c8cb8185f03ce78f09686dc7e22f524
gabirel <gabrielgualbertooliveira@gmail.com>
2025-10-17 22:00:17 -0300

    feat: create task entity

---
+++ ToDoList.Domain/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities
+{
+    public class Task
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+        public TaskStatus Status { get; set; }
+        
+    }
+}
---
+++ ToDoList.Domain/Enum/TaskStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Enum
+{
+    public enum TaskStatus
+    {
+        Canceled = -1,
+        Backlog,
+        Doing,
+        Complete
+    }
+}
