7e03941b5b38e7c426aaca8afc467322dfec977f
gabirel <gabrielgualbertooliveira@gmail.com>
2025-10-19 23:21:35 -0300

    feat: add db context and first migration

---
+++ ToDoList.Infrastructure/Configuration/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Infrastructure.Configuration
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Assignment> Assignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
